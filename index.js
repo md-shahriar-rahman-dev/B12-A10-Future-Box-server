f74724dfa050a537a8a11ce7f879d1e0d2aaf0cd
Md. Shahriar Rahman <152016174+SRTonmoy@users.noreply.github.com>
2025-11-11 23:03:38 +0600

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // ================== Firebase Admin ==================
 const serviceAccount = JSON.parse(fs.readFileSync('./firebaseServiceAccount.json', 'utf-8'));
